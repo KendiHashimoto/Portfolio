027d4ae063c555b6e4992513b579cb034edc84c3
KendiHashimoto <kendi.campos@hotmail.com>
2022-12-08 17:44:13 -0300

    Tailwind setup configs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,13 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      mobile: "375px",
+      tablet: "768px",
+      laptop: "976px",
+      desktop: "1440px",
+    },
+    colors: {},
     extend: {},
   },
   plugins: [],
